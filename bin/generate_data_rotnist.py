bd2f207c60e5b707a6c09ce7ebe6daa6c20547e8
Kaspermalm <kasperma@kth.se>
2024-07-05 13:50:54 +0200

    generate_data_rotnist

--- bin/generate_data_rotnist.py
+++ bin/generate_data_rotnist.py
@@ -62,7 +62,7 @@ def extract_data(filename, num):
         bytestream.read(16)
         buf = bytestream.read(28 * 28 * num)
         data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-        data = data.reshape(num, 28, 28,1) #reshape into tensor
+        data = data.reshape(num, 28, 28, 1) #reshape into tensor
     return data
 
 '''
@@ -105,18 +105,34 @@ def expand_training_data(images, labels, T=10):
         bg_value = 0 # this is regarded as background's value black
         #print(x)
         image = np.reshape(x, (-1, 28))
+        
+        padded_image = np.pad(image, ((10, 10), (10, 10)), mode='constant', constant_values=bg_value)
+        
+        scl_down_img = (padded_image - (255/2))/255
+        
         #time.sleep(3)
         # print(f"Shape of img: {image.shape}")
         #time.sleep(3)
         for i in range(T-1):
             # rotate the image with random degree
             angle = int(360/T)*(i+1)
-            new_img = ndimage.rotate(image,angle,reshape=False, cval=bg_value)
-            new_img2 = np.reshape(new_img,(28,28,1))
+            rotated_padded_img = ndimage.rotate(scl_down_img, angle, reshape=False, cval=bg_value, order=3)
+            #new_img2 = np.reshape(new_img,(28,28,1))
             # print(f"Shape of rotated img: {new_img2.shape}")
 
+            crop_x1 = (rotated_padded_img.shape[0] - 28) // 2
+            crop_y1 = (rotated_padded_img.shape[1] - 28) // 2
+            new_img = rotated_padded_img[crop_x1:crop_x1 + 28, crop_y1:crop_y1 + 28]
+
+            
+
+            new_img_scl_up = (new_img * 255) + (255/2)
+
+            #new_img_scl_up[new_img < 0.1] = bg_value
+
             # register new training data
-            expanded_images.append(new_img2)
+            expanded_images.append(np.reshape(new_img_scl_up, (28, 28, 1)))
+            #expanded_images.append(np.reshape(new_img_scl_up, 784))
             expanded_labels.append(y)
 
     # return them as arrays

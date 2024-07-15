b17df72234948c8df8be16618b3a00f979568712
sreyan-ghosh <sreyan2000@gmail.com>
2024-07-15 15:03:39 +0000

    added/fixed rotnist model generators

--- src/rotnist_vae_separated.py
+++ src/rotnist_vae_separated.py
@@ -39,15 +39,12 @@ class RotatedMNISTDataset(Dataset):
         return set_counts
 
 class VAE(nn.Module):
-    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=9):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=32):
         super(VAE, self).__init__()
 
-        # Encoder
         self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.fc2_mu = nn.Linear(hidden_dim, latent_dim)
         self.fc2_logvar = nn.Linear(hidden_dim, latent_dim)
-
-        # Decoder
         self.fc3 = nn.Linear(latent_dim, hidden_dim)
         self.fc4 = nn.Linear(hidden_dim, input_dim)
 
@@ -71,20 +68,45 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
     
-def loss_function(recon_x, x, mu, logvar):
+def vae_loss_function(recon_x, x, mu, logvar):
     BCE = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE + KLD
 
+class AE(nn.Module):
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=32):
+        super(AE, self).__init__()
 
-def train(epoch):
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, latent_dim)
+        self.fc3 = nn.Linear(latent_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, input_dim)
+
+    def encode(self, x):
+        h1 = torch.relu(self.fc1(x))
+        z = self.fc2(h1)
+        return z
+
+    def decode(self, z):
+        h3 = torch.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        z = self.encode(x)
+        return self.decode(z)
+
+def ae_loss_function(recon_x, x):
+  BCE = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
+  return BCE
+
+def vae_train(model, device, train_loader, optimizer, epoch):
     model.train()
     train_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        loss = vae_loss_function(recon_batch, data, mu, logvar)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
@@ -92,21 +114,47 @@ def train(epoch):
             print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} '
                   f'({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item() / len(data):.6f}')
 
-    print(f'====> Epoch: {epoch} Average loss: {train_loss / len(train_loader.dataset):.4f}')
+    avg_loss = train_loss / len(train_loader.dataset)
+    print(f'====> Epoch: {epoch} Average loss: {avg_loss:.4f}')
+    return avg_loss
 
-def encode_data(model, data_loader, device):
+def ae_train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+#       data = data.view(data.size(0), -1).to(device)  # Flatten the data
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch = model(data)
+        loss = ae_loss_function(recon_batch, data)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} '
+                  f'({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item() / len(data):.6f}')
+
+    avg_loss = train_loss / len(train_loader.dataset)
+    print(f'====> Epoch: {epoch} Average loss: {avg_loss:.4f}')
+    return avg_loss
+
+def encode_data(model, data_loader, model_type, device):
     model.eval()
-    encoded_mu = []
-    encoded_logvar = []
+    latent_array = []
     original_images_paths = []
     with torch.no_grad():
         for data, paths in data_loader:
             data = data.to(device)
-            mu, logvar = model.encode(data)
-            encoded_mu.append(mu)
-            encoded_logvar.append(logvar)
+            if model_type == 'vae':
+                mu, logvar = model.encode(data)
+                z = model.reparameterize(mu, logvar)
+            elif model_type == 'ae':
+                z = model.encode(data)
+            latent_array.append(z)
             original_images_paths.extend(paths)
-    return torch.cat(encoded_mu), torch.cat(encoded_logvar), original_images_paths
+    concat_latent_array = torch.cat(latent_array)
+    print(f"concat latent array shape: {concat_latent_array.shape}")
+    return concat_latent_array, original_images_paths
 
 def decode_data(model, z, device):
     model.eval()
@@ -115,19 +163,19 @@ def decode_data(model, z, device):
         decoded = model.decode(z)
     return decoded
 
-def run_encoding(encoded_dir=None):
+def run_encoding(model, device, train_loader, model_type, encoded_dir):
     # Save encoded data
-    encoded_mu, encoded_logvar, original_images_paths = encode_data(model, train_loader, device)
+    latent_array, original_images_paths = encode_data(model, train_loader, model_type, device)
     os.makedirs(encoded_dir, exist_ok=True)
 
     set_index = 1
-    for i in range(encoded_mu.size(0)):
+    for i in range(latent_array.size(0)):
         set_image_index = (i % T) + 1
-        torch.save((encoded_mu[i], encoded_logvar[i], original_images_paths[i]), os.path.join(encoded_dir, f'enc_img_{set_index}_{set_image_index}.pt'))
+        torch.save((latent_array[i], original_images_paths[i]), os.path.join(encoded_dir, f'enc_img_{set_index}_{set_image_index}.pt'))
         if set_image_index == T:
             set_index += 1
 
-def run_decoding(encoded_dir):
+def run_decoding(model, device, encoded_dir):
     # Load encoded data with new name format
     encoded_images = []
     for set_index in range(1, (len(glob.glob(os.path.join(encoded_dir, '*.pt'))) // T) + 1):
@@ -135,28 +183,27 @@ def run_decoding(encoded_dir):
             encoded_images.append(torch.load(os.path.join(encoded_dir, f'enc_img_{set_index}_{set_image_index}.pt')))
 
     # Load encoded data and decode
-    encoded_mu_loaded = torch.stack([img[0] for img in encoded_images])
-    encoded_logvar_loaded = torch.stack([img[1] for img in encoded_images])
-    original_images_paths_loaded = [img[2] for img in encoded_images]
+    latent_array_loaded = torch.stack([img[0] for img in encoded_images])
+    original_images_paths_loaded = [img[1] for img in encoded_images]
 
-    # Reparameterize
-    z = model.reparameterize(encoded_mu_loaded, encoded_logvar_loaded)
-
-    # Decode
-    decoded_images = decode_data(model, z, device)
+    print(f"loaded latent array shape: {latent_array_loaded.shape}")
+    with torch.no_grad():
+        # Decode
+        z = latent_array_loaded
+        decoded_images = decode_data(model, z, device)
+        print(f"decoded images shape: {decoded_images.shape}")
     
     return decoded_images, original_images_paths_loaded
 
 # Save reconstructed images to PDF
-def save_reconstructed_images(original_images_paths, decoded_images, filename='reconstructed_images_enc_dec.pdf'):
-    def sort_key(path):
-        basename = os.path.basename(path).split('.')[0]
-        set_number, image_number = map(int, basename.split('_'))
-        return set_number, image_number
+def save_reconstructed_images(original_images_paths, decoded_images, filename='reconstructed_images.pdf'):
+    # def sort_key(path):
+    #     basename = os.path.basename(path).split('.')[0]
+    #     set_number, image_number = map(int, basename.split('_'))
+    #     return set_number, image_number
 
-    original_images_paths.sort(key=sort_key)
+    # original_images_paths.sort(key=sort_key)
     
-    model.eval()
     with torch.no_grad():
         with PdfPages(filename) as pdf:
             for i in range(min(len(decoded_images), 5)):  # Save only 5 images
@@ -170,21 +217,24 @@ def save_reconstructed_images(original_images_paths, decoded_images, filename='r
                 plt.axis('off')
                 # Reconstructed Image
                 plt.subplot(1, 2, 2)
-                plt.imshow(decoded_images[i].to(device).view(28, 28), cmap='gray')
+                plt.imshow(decoded_images[i].cpu().view(28, 28), cmap='gray')
                 plt.title('Reconstructed')
                 plt.axis('off')
                 pdf.savefig()
                 plt.close()
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Input arguments related to creating a dataset for ROTNIST")
-    parser.add_argument("--mode", help="Enter 'encode' or 'decode' mode", type=str, default="decode")
+    parser.add_argument("--mode", help="Enter 'encode' or 'decode' mode", type=str, default="train")
+    parser.add_argument("--model_type", help="Enter 'ae' or 'vae' model", type=str, default="ae")
     parser.add_argument("--output_path", help="Enter full path to store the data file", type=str, default='data/encoded_data/')
+    parser.add_argument("--saved_model_path", help="Enter full path to save the AR/VAE model", type=str, default='models/rotnist_models/')
 
     args = parser.parse_args() 
     mode = args.mode
+    model_type = args.model_type
     enc_img_output_dir = args.output_path
+    saved_model_path = args.saved_model_path
     
     # Define transforms
     transform = transforms.Compose([
@@ -199,16 +249,36 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    if model_type.lower() == 'vae':
+        model = VAE().to(device)
+        train = vae_train
+    elif model_type.lower() == 'ae':
+        model = AE().to(device)
+        train = ae_train
 
-    model = VAE().to(device)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     
-    if mode.lower() == 'encode':
+    if mode.lower() == 'train':
         # Training loop
-        for epoch in range(1, 11):
-            train(epoch)
-        run_encoding(enc_img_output_dir)
+        num_epochs = 10  # Increase the number of epochs
+        for epoch in range(1, num_epochs + 1):
+            train_loss = train(model, device, train_loader, optimizer, epoch)
+            print(f'Epoch {epoch}, Loss: {train_loss}')
+        os.makedirs(saved_model_path, exist_ok=True)
+        torch.save(model, os.path.join(saved_model_path, f"{model_type}_model"))
+        print(f"Saved model to: {saved_model_path}")
+
+    elif mode.lower() == 'encode':
+        model = torch.load(os.path.join(saved_model_path, f"{model_type}_model"))
+        run_encoding(model, device, train_loader, model_type, enc_img_output_dir)
+        print(f"Saved encodings to: {enc_img_output_dir}")
     
     elif mode.lower() == 'decode':
-        decoded_images, original_image_path_loaded = run_decoding(enc_img_output_dir)
+        model = torch.load(os.path.join(saved_model_path, f"{model_type}_model"))
+        decoded_images, original_image_path_loaded = run_decoding(model, device, enc_img_output_dir)
         save_reconstructed_images(original_image_path_loaded, decoded_images)
+        print("Decoded from latent space succesfully. Check plots!")
+
+    else:
+        print("Invalid mode. Please select 'train', 'encode', or 'decode'.")
